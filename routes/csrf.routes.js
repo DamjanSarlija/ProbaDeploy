97751c609f1b12c1d7d1cc6c342ff42473b5c536
DamjanSarlija <148581555+DamjanSarlija@users.noreply.github.com>
2024-11-08 18:39:55 +0100

    Update csrf.routes.js

--- routes/csrf.routes.js
+++ routes/csrf.routes.js
@@ -8,12 +8,11 @@ router.get("/", async (req, res) => {
     }
     
     const client = new Client({
-        user: "postgres",
-        password: "bazepodataka",
-        host: "localhost",
-        port: "5432",
-        database: "Web_Lab2_Database"
-    })
+  connectionString: process.env.DATABASE_URL, // preferred in production
+  ssl: {
+    rejectUnauthorized: false // Required for external connections on Render
+  }
+});
 
     await client.connect();
 
@@ -37,12 +36,11 @@ router.get("/placanje", async (req, res) => {
         res.redirect("/")
     } else {
         const client = new Client({
-            user: "postgres",
-            password: "bazepodataka",
-            host: "localhost",
-            port: "5432",
-            database: "Web_Lab2_Database"
-        })
+  connectionString: process.env.DATABASE_URL, // preferred in production
+  ssl: {
+    rejectUnauthorized: false // Required for external connections on Render
+  }
+});
     
         await client.connect();
 
@@ -96,12 +94,11 @@ router.get("/dohvatiPodatkeOSesiji", (req, res) => {
 
 router.get("/izbrisiSve", async (req, res) => {
     const client = new Client({
-        user: "postgres",
-        password: "bazepodataka",
-        host: "localhost",
-        port: "5432",
-        database: "Web_Lab2_Database"
-    })
+  connectionString: process.env.DATABASE_URL, // preferred in production
+  ssl: {
+    rejectUnauthorized: false // Required for external connections on Render
+  }
+});
 
     await client.connect();
     await client.query("DELETE FROM transakcije")
@@ -112,12 +109,11 @@ router.get("/izbrisiSve", async (req, res) => {
 
 router.get("/izbrisiSvaUpozorenja", async (req, res) => {
     const client = new Client({
-        user: "postgres",
-        password: "bazepodataka",
-        host: "localhost",
-        port: "5432",
-        database: "Web_Lab2_Database"
-    })
+  connectionString: process.env.DATABASE_URL, // preferred in production
+  ssl: {
+    rejectUnauthorized: false // Required for external connections on Render
+  }
+});
 
     await client.connect();
     await client.query("DELETE FROM upozorenja")
